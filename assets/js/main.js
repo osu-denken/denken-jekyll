f8c8701b9fcbb1c311b85d10557c506d994e9dba
Mizuto, Kaminakano <mizuto1127no01@gmail.com>
2025-11-03 01:27:49 +0000

    feat: add autolink

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function updateScrolledClass() {
-    if (document.documentElement.scrollHeight > window.innerHeight) {
-      document.body.classList.add('scrolled');
-    } else {
-      document.body.classList.remove('scrolled');
-    }
+// サイズに基づいてスクロールの調整
+function applyScrolledClass() {
+  if (document.documentElement.scrollHeight > window.innerHeight) {
+    document.body.classList.add('scrolled');
+  } else {
+    document.body.classList.remove('scrolled');
   }
-  updateScrolledClass();
-  window.addEventListener('resize', updateScrolledClass);
+}
+
+// .page-content内のURLを検出してリンクを有効化
+function applyAutolink() {
+  const urlPattern = /(https?:\/\/[^\s<>"']+)/g;
+  const internalLinkPattern = new RegExp(`^https?://${window.location.host}`); // 内部リンク用パターン
+
+  const pageContent = document.querySelector('.page-content');
+  if (!pageContent) return;
+
+  pageContent.innerHTML = pageContent.innerHTML.replace(urlPattern, function(url) {
+    const isInternal = internalLinkPattern.test(url);
+    return `<a href="${url}"` + isInternal ? `` : ` target="_blank"` + ` rel="noopener noreferrer">${url}</a>`;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  applyScrolledClass();
+  window.addEventListener('resize', applyScrolledClass);
+
+  applyAutolink();
 });
